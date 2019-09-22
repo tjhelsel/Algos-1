c35957d4b45bfa0e1eefd952be890f7083b32337
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2019-09-22 10:14:46 -0400

    tweaked _test.js by including lowestTest and highestTest

--- _test.js
+++ _test.js
@@ -1,11 +1,13 @@
 const equals = require('./_equality-checker');
 
-function test (output, expected, testNum) {
-  console.log(
-    equals(output, expected)
-      ? `TEST ${testNum[0]} PASSED`
-      : `TEST ${testNum[0]} FAILED: EXPECTED ${expected} BUT GOT ${output}`
-  );
+function test (output, expected, testNum, lowestTest = 0, highestTest = Infinity) {
+  if (testNum >= lowestTest && testNum <= highestTest) {
+    console.log(
+      equals(output, expected)
+        ? `TEST ${testNum[0]} PASSED`
+        : `TEST ${testNum[0]} FAILED: EXPECTED ${expected} BUT GOT ${output}`
+    );
+  }
   testNum[0]++;
 };
 
