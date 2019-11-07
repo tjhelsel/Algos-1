251ca72a09c57ee4dd88cea14a772e18d1dccd02
tjhelsel <terence.helsel@gmail.com>
2019-11-06 19:14:58 -0500

    optimized BT-path-to-node solution

--- BT-path-to-node.js
+++ BT-path-to-node.js
@@ -33,31 +33,19 @@ class BinaryTree {
   // SWITCHING BETWEEN SOLUTIONS:
   findPathToTarget (target) { return this.solution_1(target); }
 
-  solution_1 (target) {
-
-    // SOLUTION 1 [O(n) time (n is the number of nodes in the tree), O(d) space (d is the depth of the tree)]:
-    // this solution uses DFS backtracking. we start with an initial output 'path' of []. we write a recursive 'go' function that returns a boolean whether a valid path exists, given
-    // the current path described in output. if the current node does not exist, return false. otherwise, add the current node's value to the output path. if the current node's value
-    // is the target, return true. otherwise, recurse on node.left and node.right. if either are those ultimately result in true, then return true (which 'bubbles up' the truthy
-    // result). if, on the other hand, all of those fail, then we have reached a dead end, so we pop the current value from the output path and return false. since at every step we
-    // direclty modify the output 'path' object, all we have to do is invoke go() at the start, and then we can ultimately simply return the output object.
-
-    const output = [];
-
-    const go = (node = this) => {   // if writing this out as a separate function, use "root" instead of "this"
-      if (!node) return false;      // base case false: if no more nodes on this path
-      output.push(node.value);      // try this path by pushing the current node into the path
-      if (
-        node.value === target       // base case true: if current node is your target
-        || go(node.left)            // recursive case true: if continuing to your left ultimately finds the target
-        || go(node.right)           // recursive case true: if continuing to your right ultimately finds the target
-      ) return true;                // 'bubble up' the truthy value for recursion
-      output.pop();                 // recursive case false: going any further would be a dead end, so pop out the current node from the path
-      return false;                 // 'bubble up' the falsy value for recursion
+  solution_1 (target, path = []) {
+    path.push(this.value)
+    if (this.value === target) return path;
+    if (this.left) {
+      const leftPath = this.left.solution_1(target, path)
+      if (leftPath.length > 0) return leftPath;
     }
-
-    go();                           // simply invoke to start the process. we don't care what it returns - we just want it to produce the necessary side effects on the output object
-    return output;                  // after invoking go(), we just need to return the output object
+    if (this.right) {
+      const rightPath = this.right.solution_1(target, path)
+      if (rightPath.length > 0) return rightPath;
+    }
+    path.pop()
+    return [];
   }
 
 }
@@ -104,9 +92,9 @@ input = {
     .insert('B', 'C', true)               // first .insert must END with 'true' argument
     .insert(false, 'D', 'E', 'F', 'G')    // subsequent .inserts must START with 'false' argument...
     .insert(true, 'H'),                   // ...except the last .insert which must START with 'true' argument
-  target: 'H',
+  target: 'G',
 };
-expected = ['A', 'B', 'D', 'H'];
+expected = ['A', 'C', 'G'];
 test(func.bind(input.root), {target: input.target}, expected, testNum, lowestTest, highestTest);
 
 // Test case 3
